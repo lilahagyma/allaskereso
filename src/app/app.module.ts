c80c4cc00cbc74a9ff072511f89009e43118a14e
Varga Dániel <h165922@stud.u-szeged.hu>
2023-05-02 18:01:20 +0200

    Komponensek létrehozása

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SeekerComponent } from './pages/seeker/seeker.component';
+import { StatsComponent } from './pages/stats/stats.component';
+import { NewJobComponent } from './pages/new-job/new-job.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    SeekerComponent,
+    StatsComponent,
+    NewJobComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'allaskereso-5bd68',
+    appId: '1:114249341836:web:1574d06d351d4ca54a14a9',
+    storageBucket: 'allaskereso-5bd68.appspot.com',
+    apiKey: 'AIzaSyDIOSC6bBYZYZp1G_pkOVKowhdLO_34fOU',
+    authDomain: 'allaskereso-5bd68.firebaseapp.com',
+    messagingSenderId: '114249341836',
+  },};
